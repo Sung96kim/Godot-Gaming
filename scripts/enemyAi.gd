5f15993016c7374f5d538f8bbf764436d75f3c54
Sung <sung96kim@gmail.com>
2022-01-24 17:47:58 -0500

    ADD enemy ai with player detection

--- scripts/enemyAi.gd
+++ scripts/enemyAi.gd
@@ -3,9 +3,8 @@ extends KinematicBody2D
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var WANDER_RANGE = 4
+
 var knockback = Vector2.ZERO
 var velocity = Vector2.ZERO
 
@@ -19,8 +18,13 @@ var state = IDLE
 
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var sprite = $AnimatedSprite
+onready var wanderController = $WanderController
+onready var softCollision = $SoftCollision
+onready var stats = $Stats
 
-# Called when the node enters the scene tree for the first time.
+func _ready():
+	state = pick_new_state([IDLE, WANDER])
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
@@ -28,21 +32,55 @@ func _physics_process(delta):
 	match state: 
 		IDLE: 
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
 			
+			if wanderController.get_time_left() == 0: 
+				state = pick_new_state([IDLE, WANDER])
+				wanderController.timer_start(rand_range(1, 3))
 		WANDER: 
-			pass
-		
+			seek_player()
+			if wanderController.get_time_left() == 0: 
+				state = pick_new_state([IDLE, WANDER])
+				wanderController.timer_start(rand_range(1, 3))
+			
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			sprite.flip_h = velocity.x < 0
+						
+			if global_position.distance_to(wanderController.target_position) <= WANDER_RANGE:
+				state = pick_new_state([IDLE, WANDER])
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null: 
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+		
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 400
 		
 	velocity = move_and_slide(velocity)
 
 func seek_player(): 
 	if playerDetectionZone.can_see_player(): 
 		state = CHASE
+		
+func pick_new_state(stateList):
+	stateList.shuffle()
+	return stateList.pop_front()
 
 func _on_HurtBox_area_entered(area):
-	queue_free() # Replace with function body.
+#	var knockback_vector = area.knockback_vector
+
+# UNCOMMENT and delete other line once player is working
+#	stats.health -= area.damage
+	
+	stats.health -= area.damage
+	knockback = Vector2.RIGHT * 100
+
+func _on_Stats_no_health():
+	queue_free()
